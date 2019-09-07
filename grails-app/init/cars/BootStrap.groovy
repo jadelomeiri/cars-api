5bd93ba39f99fc4ff39c4092ad3a8a0606bd9750
Jad El Omeiri <Jad@somertonsc.com>
2019-09-07 13:30:24 +0100

    added postgres for persistence + added data generation on start when using h2

--- grails-app/init/cars/BootStrap.groovy
+++ grails-app/init/cars/BootStrap.groovy
@@ -1,8 +1,16 @@
 package cars
 
+import grails.util.Environment
+
 class BootStrap {
 
     def init = { servletContext ->
+
+        if (Environment.current == Environment.DEVELOPMENT) {
+            Car car1 = new Car(make: "make 1", model: "model 1", colour: "colour 1", year: 2001).save(flush: true)
+            Car car2 = new Car(make: "make 2", model: "model 1", colour: "colour 1", year: 2001).save(flush: true)
+            Car car3 = new Car(make: "make 3", model: "model 1", colour: "colour 1", year: 2001).save(flush: true)
+        }
     }
     def destroy = {
     }
